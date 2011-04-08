924e9c7a7b7d0251047ebc29358900cd30699206
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2011-04-08 16:39:17 +0000

    initial import

---
+++ elk-reasoner/src/main/java/org/semanticweb/elk/reasoner/ELKInverseObjectPropertiesAxiom.java
@@ -0,0 +1,44 @@
+/*
+ * #%L
+ * elk-reasoner
+ * 
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2011 Oxford University Computing Laboratory
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/**
+ * @author Yevgeny Kazakov, Apr 8, 2011
+ */
+package org.semanticweb.elk.reasoner;
+
+/**
+ * @author Yevgeny Kazakov
+ *
+ */
+public class ELKInverseObjectPropertiesAxiom extends ELKObjectPropertyAxiom {
+
+	/* (non-Javadoc)
+	 * @see org.semanticweb.elk.reasoner.ELKObjectPropertyAxiom#accept(org.semanticweb.elk.reasoner.ELKObjectPropertyAxiomVisitor)
+	 */
+	@Override
+	public <O> O accept(ELKObjectPropertyAxiomVisitor<O> visitor) {
+		
+		return visitor.visit(this);
+		
+	}
+
+}
