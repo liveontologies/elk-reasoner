1ac497608828c7a0ad61d1b637d474731cfc5a51
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2011-06-06 18:24:21 +0000

    Statistic class to print memory usage and so on.

---
+++ src/main/java/org/semanticweb/elk/util/Statistics.java
@@ -0,0 +1,53 @@
+/*
+ * #%L
+ * elk-reasoner
+ * 
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2011 Department of Computer Science, University of Oxford
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/**
+ * @author Yevgeny Kazakov, Jun 6, 2011
+ */
+package org.semanticweb.elk.util;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Yevgeny Kazakov
+ * 
+ */
+public class Statistics {
+
+	// Declare the size
+	final static int megaBytes = 1024 * 1024;
+
+	public static void logMemoryUsage(Logger logger) {
+
+		// Getting the runtime reference from system
+		Runtime runtime = Runtime.getRuntime();
+
+		// Logging the memory statistics
+		if (logger.isDebugEnabled()) {
+			logger.debug("Memory (MB) Used/Total/Max: "
+					+ (runtime.totalMemory() - runtime.freeMemory())
+					/ megaBytes + "/" + runtime.totalMemory() / megaBytes + "/"
+					+ runtime.maxMemory() / megaBytes);
+		}
+	}
+
+}
