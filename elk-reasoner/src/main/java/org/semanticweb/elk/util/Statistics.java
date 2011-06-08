a6ee22870cf0096d398a00daac36f414773a0beb
Markus Kroetzsch <mak@aifb.uni-karlsruhe.de>
2011-06-08 15:54:13 +0000

    +documentation

--- elk-reasoner/src/main/java/org/semanticweb/elk/util/Statistics.java
+++ elk-reasoner/src/main/java/org/semanticweb/elk/util/Statistics.java
@@ -20,9 +20,7 @@
  * limitations under the License.
  * #L%
  */
-/**
- * @author Yevgeny Kazakov, Jun 6, 2011
- */
+
 package org.semanticweb.elk.util;
 
 import org.apache.log4j.Level;
@@ -30,6 +28,9 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.Priority;
 
 /**
+ * Collection of several static methods that help in taking and logging
+ * statistical information.
+ * 
  * @author Yevgeny Kazakov
  * @author Markus Kroetzsch
  */
@@ -37,36 +38,87 @@ public class Statistics {
 
 	// Declare the size
 	final static int megaBytes = 1024 * 1024;
-	
+
+	/**
+	 * Log the start of a particular operation with INFO priority. This method
+	 * should be used for long-running tasks and is mainly intended. Multiple
+	 * threads can independently log operations of the same name, but
+	 * (obviously) no single thread should use the same operation name to record
+	 * the start and end of overlapping code.
+	 * 
+	 * @param operationName
+	 * @param logger
+	 */
 	public static void logOperationStart(String operationName, Logger logger) {
 		logOperationStart(operationName, logger, Level.INFO);
 	}
-	
-	public static void logOperationStart(String operationName, Logger logger, Priority priority) {
+
+	/**
+	 * Log the start of a particular operation with the given priority. This
+	 * method should be used for long-running tasks and is mainly intended.
+	 * Multiple threads can independently log operations of the same name, but
+	 * (obviously) no single thread should use the same operation name to record
+	 * the start and end of overlapping code.
+	 * 
+	 * @param operationName
+	 * @param logger
+	 * @param priority
+	 */
+	public static void logOperationStart(String operationName, Logger logger,
+			Priority priority) {
 		if (logger.isEnabledFor(priority)) {
 			logger.log(priority, operationName + " started");
-			ElkTimer timer = ElkTimer.getNamedTimer(operationName, ElkTimer.RECORD_WALLTIME);
+			ElkTimer timer = ElkTimer.getNamedTimer(operationName,
+					ElkTimer.RECORD_WALLTIME);
 			timer.reset(); // needed in case this was done before
 			timer.start();
 		}
 	}
-	
+
+	/**
+	 * Log the end of a particular operation the beginning of which has been
+	 * logged using logOperationStart(), using INFO priority.
+	 * 
+	 * @param operationName
+	 * @param logger
+	 */
 	public static void logOperationFinish(String operationName, Logger logger) {
 		logOperationFinish(operationName, logger, Level.INFO);
 	}
-	
-	public static void logOperationFinish(String operationName, Logger logger, Priority priority) {
+
+	/**
+	 * Log the end of a particular operation the beginning of which has been
+	 * logged using logOperationStart(), using the given logging priority.
+	 * 
+	 * @param operationName
+	 * @param logger
+	 * @param priority
+	 */
+	public static void logOperationFinish(String operationName, Logger logger,
+			Priority priority) {
 		if (logger.isEnabledFor(priority)) {
-			ElkTimer timer = ElkTimer.getNamedTimer(operationName, ElkTimer.RECORD_WALLTIME);
+			ElkTimer timer = ElkTimer.getNamedTimer(operationName,
+					ElkTimer.RECORD_WALLTIME);
 			timer.stop();
-			logger.log(priority, operationName + " finished in " + timer.getTotalWallTime() / 1000000 + " ms");
+			logger.log(priority, operationName + " finished in "
+					+ timer.getTotalWallTime() / 1000000 + " ms");
 		}
 	}
 
+	/**
+	 * Log the current total memory usage using DEBUG priority.
+	 * 
+	 * @param logger
+	 */
 	public static void logMemoryUsage(Logger logger) {
 		logMemoryUsage(logger, Level.DEBUG);
 	}
 
+	/**
+	 * Log the current total memory usage with the specified priority.
+	 * 
+	 * @param logger
+	 */
 	public static void logMemoryUsage(Logger logger, Priority priority) {
 		if (logger.isEnabledFor(priority)) {
 			// Getting the runtime reference from system
