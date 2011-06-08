aff60b06e4c36903dcfa2a66f4ac4c5f96e21dea
Markus Kroetzsch <mak@aifb.uni-karlsruhe.de>
2011-06-08 15:46:11 +0000

    extended ElkTimer functionality, added standard time logging functions to Statistics

--- src/main/java/org/semanticweb/elk/util/ElkTimer.java
+++ src/main/java/org/semanticweb/elk/util/ElkTimer.java
@@ -38,22 +38,51 @@ import org.apache.log4j.Priority;
  */
 public class ElkTimer {
 
+	/** Flag for indicating that no times should be taken (just count runs). */
+	public static final int RECORD_NONE = 0x00000000;
+	/** Flag for indicating that CPU time should be taken. */
+	public static final int RECORD_CPUTIME = 0x00000001;
+	/** Flag for indicating that wall clock time should be taken. */
+	public static final int RECORD_WALLTIME = 0x00000002;
+	/** Flag for indicating that all supported times should be taken. */
+	public static final int RECORD_ALL = RECORD_CPUTIME | RECORD_WALLTIME;
+
 	static final ThreadMXBean tmxb = ManagementFactory.getThreadMXBean();
 
 	static final ConcurrentHashMap<ElkTimer, ElkTimer> registeredTimers = new ConcurrentHashMap<ElkTimer, ElkTimer>();
 
 	protected final String name;
 	protected final long threadId;
+	protected final int todoFlags;
 
 	protected long currentStartCpuTime = -1;
-	protected long currentStartSystemTime = -1;
+	protected long currentStartWallTime = -1;
+	protected boolean isRunning = false;
 	protected long totalCpuTime = 0;
-	protected long totalSystemTime = 0;
+	protected long totalWallTime = 0;
 	protected int measurements = 0;
 	protected int threadCount = 0;
 
-	public ElkTimer(String name, long threadId) {
+	/**
+	 * Constructor. Every timer is identified by three things: a string name, an
+	 * integer for flagging its tasks (todos), and a thread id (long).
+	 * 
+	 * Tasks can be flagged by a disjunction of constants like RECORD_CPUTIME
+	 * and RECORD_WALLTIME. Only times for which an according flag is set will
+	 * be recorded.
+	 * 
+	 * The thread id can be the actual id of the thread that is measured, or 0
+	 * (invalid id) to not assign the timer to any thread. In this case, no CPU
+	 * time measurement is possible since Java does not allow us to measure the
+	 * total CPU time across all threads.
+	 * 
+	 * @param name
+	 * @param todoFlags
+	 * @param threadId
+	 */
+	public ElkTimer(String name, int todoFlags, long threadId) {
 		this.name = name;
+		this.todoFlags = todoFlags;
 		this.threadId = threadId;
 
 		if (!tmxb.isThreadCpuTimeEnabled()) {
@@ -65,10 +94,11 @@ public class ElkTimer {
 	 * Create a new Timer for the current thread.
 	 * 
 	 * @param name
+	 * @param todoFlags
 	 * @return
 	 */
-	static public ElkTimer getTimerForCurrentThread(String name) {
-		return new ElkTimer(name, Thread.currentThread().getId());
+	static public ElkTimer getTimerForCurrentThread(String name, int todoFlags) {
+		return new ElkTimer(name, todoFlags, Thread.currentThread().getId());
 	}
 
 	/**
@@ -99,12 +129,12 @@ public class ElkTimer {
 	}
 
 	/**
-	 * Get the total recorded system time in nanoseconds.
+	 * Get the total recorded wall clock time in nanoseconds.
 	 * 
-	 * @return recorded system time in nanoseconds
+	 * @return recorded wall time in nanoseconds
 	 */
-	public long getTotalSystemTime() {
-		return totalCpuTime;
+	public long getTotalWallTime() {
+		return totalWallTime;
 	}
 
 	/**
@@ -113,15 +143,24 @@ public class ElkTimer {
 	 * @return true if running
 	 */
 	public boolean isRunning() {
-		return currentStartSystemTime != -1;
+		return isRunning;
 	}
 
 	/**
 	 * Start the timer.
 	 */
 	public synchronized void start() {
-		currentStartCpuTime = getThreadCpuTime(threadId);
-		currentStartSystemTime = System.nanoTime();
+		if ((todoFlags & RECORD_CPUTIME) != 0) {
+			currentStartCpuTime = getThreadCpuTime(threadId);
+		} else {
+			currentStartCpuTime = -1;
+		}
+		if ((todoFlags & RECORD_WALLTIME) != 0) {
+			currentStartWallTime = System.nanoTime();
+		} else {
+			currentStartWallTime = -1;
+		}
+		isRunning = true;
 	}
 
 	/**
@@ -129,9 +168,9 @@ public class ElkTimer {
 	 */
 	public synchronized void reset() {
 		currentStartCpuTime = -1;
-		currentStartSystemTime = -1;
+		currentStartWallTime = -1;
 		totalCpuTime = 0;
-		totalSystemTime = 0;
+		totalWallTime = 0;
 	}
 
 	/**
@@ -146,19 +185,27 @@ public class ElkTimer {
 	 */
 	public synchronized long stop() {
 		long totalTime = -1;
-		long systemTime = System.nanoTime();
-		long cpuTime = getThreadCpuTime(threadId);
 
-		if (currentStartCpuTime != -1) { // was timer started?
+		if ((todoFlags & RECORD_CPUTIME) != 0 && (currentStartCpuTime != -1)) {
+			long cpuTime = getThreadCpuTime(threadId);
 			if (cpuTime != -1) { // may fail if thread already dead
 				totalTime = cpuTime - currentStartCpuTime;
 				totalCpuTime += totalTime;
 			}
-			totalSystemTime += systemTime - currentStartSystemTime;
-			currentStartSystemTime = -1;
-			currentStartCpuTime = -1;
-			measurements += 1;
 		}
+
+		if ((todoFlags & RECORD_WALLTIME) != 0 && (currentStartWallTime != -1)) {
+			long wallTime = System.nanoTime();
+			totalWallTime += wallTime - currentStartWallTime;
+		}
+
+		if (isRunning) {
+			measurements += 1;
+			isRunning = false;
+		}
+
+		currentStartWallTime = -1;
+		currentStartCpuTime = -1;
 
 		return totalTime;
 	}
@@ -184,32 +231,72 @@ public class ElkTimer {
 	 */
 	public void log(Logger logger, Priority priority) {
 		if (logger.isEnabledFor(priority)) {
+			String timerLabel;
 			if (threadId != 0) {
-				logger.log(priority, "Timer '" + name + "' (thread " + threadId
-						+ ") total over " + measurements
-						+ " measurements (ms) CPU/System: " + totalCpuTime
-						/ 1000000 + "/" + totalSystemTime / 1000000);
-			} else if (threadCount > 0) {
-				logger.log(priority, "Timer '" + name + "' (sum of "
-						+ threadCount + " threads) total over " + measurements
-						+ " measurements (ms) CPU/System: " + totalCpuTime
-						/ 1000000 + "/" + totalSystemTime / 1000000);
+				timerLabel = name + " (thread " + threadId + ")";
+			} else if (threadCount > 1) {
+				timerLabel = name + " (over " + threadCount + " threads)";
 			} else {
-				logger.log(priority, "Timer '" + name + "' (no thread) "
-						+ " total over " + measurements
-						+ " measurements (ms) System: " + totalSystemTime
-						/ 1000000);
+				timerLabel = name;
 			}
-			if (currentStartCpuTime != -1) {
-				logger.warn("Timer '" + name + "' in thread " + threadId
+
+			if (todoFlags == RECORD_NONE) {
+				logger.log(priority, "Timer " + timerLabel + " recorded "
+						+ measurements + " run(s), no times taken");
+			} else {
+				String labels = "";
+				String values = "";
+				String separator;
+
+				if ((todoFlags & RECORD_CPUTIME) != 0 && threadId != 0) {
+					labels += "CPU";
+					values += totalCpuTime / 1000000;
+					separator = "/";
+				} else {
+					separator = "";
+				}
+				if ((todoFlags & RECORD_WALLTIME) != 0) {
+					labels += separator + "Wall";
+					values += separator + totalWallTime / 1000000;
+				}
+				if ((todoFlags & RECORD_CPUTIME) != 0 && threadId != 0) {
+					labels += "/CPU avg";
+					values += "/" + (float) (totalCpuTime) / measurements
+							/ 1000000;
+				}
+				if ((todoFlags & RECORD_WALLTIME) != 0) {
+					labels += "/Wall avg";
+					values += "/" + (float) (totalWallTime) / measurements
+							/ 1000000;
+				}
+				if (threadCount > 1) {
+					if ((todoFlags & RECORD_CPUTIME) != 0 && threadId != 0) {
+						labels += "/CPU per thread";
+						values += "/" + (float) (totalCpuTime) / threadCount
+								/ 1000000;
+					}
+					if ((todoFlags & RECORD_WALLTIME) != 0) {
+						labels += "/Wall per thread";
+						values += "/" + (float) (totalWallTime) / threadCount
+								/ 1000000;
+					}
+				}
+
+				logger.log(priority, "Time for " + timerLabel + " for "
+						+ measurements + " run(s) " + labels + " (ms): "
+						+ values);
+			}
+
+			if (isRunning) {
+				logger.warn("Timer " + timerLabel
 						+ " logged while it was still running");
 			}
 		}
 	}
 
 	/**
-	 * Start a timer of the given string name for the current thread. If no such
-	 * timer exists yet, then it will be newly created.
+	 * Start a timer of the given string name for all todos and the current
+	 * thread. If no such timer exists yet, then it will be newly created.
 	 * 
 	 * @param timerName
 	 *            the name of the timer
@@ -224,12 +311,39 @@ public class ElkTimer {
 	 * 
 	 * @param timerName
 	 *            the name of the timer
+	 */
+	public static void startNamedTimer(String timerName, int todoFlags) {
+		getNamedTimer(timerName, todoFlags).start();
+	}
+
+	/**
+	 * Start a timer of the given string name for the current thread. If no such
+	 * timer exists yet, then it will be newly created.
+	 * 
+	 * @param timerName
+	 *            the name of the timer
+	 * @param todoFlags
 	 * @param threadId
 	 *            of the thread to track, or 0 if only system clock should be
 	 *            tracked
 	 */
-	public static void startNamedTimer(String timerName, long threadId) {
-		getNamedTimer(timerName, threadId).start();
+	public static void startNamedTimer(String timerName, int todoFlags,
+			long threadId) {
+		getNamedTimer(timerName, todoFlags, threadId).start();
+	}
+
+	/**
+	 * Stop a timer of the given string name for all todos and the current
+	 * thread. If no such timer exists, -1 will be returned. Otherwise the
+	 * return value is the CPU time that was measured.
+	 * 
+	 * @param timerName
+	 *            the name of the timer
+	 * @return CPU time if timer existed and was running, and -1 otherwise
+	 */
+	public static long stopNamedTimer(String timerName) {
+		return stopNamedTimer(timerName, RECORD_ALL, Thread.currentThread()
+				.getId());
 	}
 
 	/**
@@ -239,10 +353,12 @@ public class ElkTimer {
 	 * 
 	 * @param timerName
 	 *            the name of the timer
+	 * @param todoFlags
 	 * @return CPU time if timer existed and was running, and -1 otherwise
 	 */
-	public static long stopNamedTimer(String timerName) {
-		return stopNamedTimer(timerName, Thread.currentThread().getId());
+	public static long stopNamedTimer(String timerName, int todoFlags) {
+		return stopNamedTimer(timerName, todoFlags, Thread.currentThread()
+				.getId());
 	}
 
 	/**
@@ -252,13 +368,15 @@ public class ElkTimer {
 	 * 
 	 * @param timerName
 	 *            the name of the timer
+	 * @param todoFlags
 	 * @param threadId
 	 *            of the thread to track, or 0 if only system clock should be
 	 *            tracked
 	 * @return CPU time if timer existed and was running, and -1 otherwise
 	 */
-	public static long stopNamedTimer(String timerName, long threadId) {
-		ElkTimer key = new ElkTimer(timerName, threadId);
+	public static long stopNamedTimer(String timerName, int todoFlags,
+			long threadId) {
+		ElkTimer key = new ElkTimer(timerName, todoFlags, threadId);
 		if (registeredTimers.containsKey(key)) {
 			return registeredTimers.get(key).stop();
 		} else {
@@ -267,14 +385,26 @@ public class ElkTimer {
 	}
 
 	/**
-	 * Reset a timer of the given string name for the current thread. If no such
-	 * timer exists yet, then it will be newly created.
+	 * Reset a timer of the given string name for all todos and the current
+	 * thread. If no such timer exists yet, then it will be newly created.
 	 * 
 	 * @param timerName
 	 *            the name of the timer
 	 */
 	public static void resetNamedTimer(String timerName) {
 		getNamedTimer(timerName).reset();
+	}
+
+	/**
+	 * Reset a timer of the given string name for the current thread. If no such
+	 * timer exists yet, then it will be newly created.
+	 * 
+	 * @param timerName
+	 *            the name of the timer
+	 * @param todoFlags
+	 */
+	public static void resetNamedTimer(String timerName, int todoFlags) {
+		getNamedTimer(timerName, todoFlags).reset();
 	}
 
 	/**
@@ -287,20 +417,37 @@ public class ElkTimer {
 	 *            of the thread to track, or 0 if only system clock should be
 	 *            tracked
 	 */
-	public static void resetNamedTimer(String timerName, long threadId) {
-		getNamedTimer(timerName, threadId).reset();
+	public static void resetNamedTimer(String timerName, int todoFlags,
+			long threadId) {
+		getNamedTimer(timerName, todoFlags, threadId).reset();
 	}
 
 	/**
-	 * Get a timer of the given string name for the current thread. If no such
-	 * timer exists yet, then it will be newly created.
+	 * Get a timer of the given string name that takes all possible times
+	 * (todos) for the current thread. If no such timer exists yet, then it will
+	 * be newly created.
 	 * 
 	 * @param timerName
 	 *            the name of the timer
 	 * @return timer
 	 */
 	public static ElkTimer getNamedTimer(String timerName) {
-		return getNamedTimer(timerName, Thread.currentThread().getId());
+		return getNamedTimer(timerName, RECORD_ALL, Thread.currentThread()
+				.getId());
+	}
+
+	/**
+	 * Get a timer of the given string name and todos for the current thread. If
+	 * no such timer exists yet, then it will be newly created.
+	 * 
+	 * @param timerName
+	 *            the name of the timer
+	 * @param todoFlags
+	 * @return timer
+	 */
+	public static ElkTimer getNamedTimer(String timerName, int todoFlags) {
+		return getNamedTimer(timerName, todoFlags, Thread.currentThread()
+				.getId());
 	}
 
 	/**
@@ -314,8 +461,9 @@ public class ElkTimer {
 	 *            tracked
 	 * @return timer
 	 */
-	public static ElkTimer getNamedTimer(String timerName, long threadId) {
-		ElkTimer key = new ElkTimer(timerName, threadId);
+	public static ElkTimer getNamedTimer(String timerName, int todoFlags,
+			long threadId) {
+		ElkTimer key = new ElkTimer(timerName, todoFlags, threadId);
 		registeredTimers.putIfAbsent(key, key);
 		return registeredTimers.get(key);
 	}
@@ -332,23 +480,25 @@ public class ElkTimer {
 		long totalSystemTime = 0;
 		int measurements = 0;
 		int threadCount = 0;
+		int todoFlags = RECORD_NONE;
 		ElkTimer previousTimer = null;
 		for (Map.Entry<ElkTimer, ElkTimer> entry : registeredTimers.entrySet()) {
 			if (entry.getValue().name.equals(timerName)) {
 				previousTimer = entry.getValue();
 				threadCount += 1;
 				totalCpuTime += previousTimer.totalCpuTime;
-				totalSystemTime += previousTimer.totalSystemTime;
+				totalSystemTime += previousTimer.totalWallTime;
 				measurements += previousTimer.measurements;
+				todoFlags |= previousTimer.todoFlags;
 			}
 		}
 
 		if (threadCount == 1) {
 			return previousTimer;
 		} else {
-			ElkTimer result = new ElkTimer(timerName, 0);
+			ElkTimer result = new ElkTimer(timerName, todoFlags, 0);
 			result.totalCpuTime = totalCpuTime;
-			result.totalSystemTime = totalSystemTime;
+			result.totalWallTime = totalSystemTime;
 			result.measurements = measurements;
 			result.threadCount = threadCount;
 			return result;
@@ -358,7 +508,7 @@ public class ElkTimer {
 	public static void logAllNamedTimers(String timerName, Logger logger) {
 		logAllNamedTimers(timerName, logger, Level.DEBUG);
 	}
-	
+
 	public static void logAllNamedTimers(String timerName, Logger logger,
 			Priority priority) {
 		for (Map.Entry<ElkTimer, ElkTimer> entry : registeredTimers.entrySet()) {
@@ -372,7 +522,9 @@ public class ElkTimer {
 	public int hashCode() {
 		int nameHash = name.hashCode();
 		int threadIdHash = new Long(threadId).hashCode();
-		return HashGenerator.combineListHash(nameHash, threadIdHash);
+		int todoFlagsHash = new Integer(todoFlags).hashCode();
+		return HashGenerator.combineListHash(nameHash, todoFlagsHash,
+				threadIdHash);
 	}
 
 	@Override
@@ -384,6 +536,7 @@ public class ElkTimer {
 		} else if (getClass() != obj.getClass()) {
 			return false;
 		} else if (threadId == ((ElkTimer) obj).threadId
+				&& todoFlags == ((ElkTimer) obj).todoFlags
 				&& name.equals(((ElkTimer) obj).name)) {
 			return true;
 		} else {
--- src/main/java/org/semanticweb/elk/util/Statistics.java
+++ src/main/java/org/semanticweb/elk/util/Statistics.java
@@ -31,11 +31,37 @@ import org.apache.log4j.Priority;
 
 /**
  * @author Yevgeny Kazakov
+ * @author Markus Kroetzsch
  */
 public class Statistics {
 
 	// Declare the size
 	final static int megaBytes = 1024 * 1024;
+	
+	public static void logOperationStart(String operationName, Logger logger) {
+		logOperationStart(operationName, logger, Level.INFO);
+	}
+	
+	public static void logOperationStart(String operationName, Logger logger, Priority priority) {
+		if (logger.isEnabledFor(priority)) {
+			logger.log(priority, operationName + " started");
+			ElkTimer timer = ElkTimer.getNamedTimer(operationName, ElkTimer.RECORD_WALLTIME);
+			timer.reset(); // needed in case this was done before
+			timer.start();
+		}
+	}
+	
+	public static void logOperationFinish(String operationName, Logger logger) {
+		logOperationFinish(operationName, logger, Level.INFO);
+	}
+	
+	public static void logOperationFinish(String operationName, Logger logger, Priority priority) {
+		if (logger.isEnabledFor(priority)) {
+			ElkTimer timer = ElkTimer.getNamedTimer(operationName, ElkTimer.RECORD_WALLTIME);
+			timer.stop();
+			logger.log(priority, operationName + " finished in " + timer.getTotalWallTime() / 1000000 + " ms");
+		}
+	}
 
 	public static void logMemoryUsage(Logger logger) {
 		logMemoryUsage(logger, Level.DEBUG);
