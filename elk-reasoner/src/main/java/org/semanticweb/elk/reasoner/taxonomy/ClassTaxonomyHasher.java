2ac43b0c1af9dd6ca04b7e9b48d9435cfb1296cd
Markus Kroetzsch <mak@aifb.uni-karlsruhe.de>
2012-03-14 15:24:44 +0000

    implemented a simple hash cache to ensure feasible performance even if a node is equivalent to hundreds of thousands of classes (as may happen for owl:Nothing in case of modelling errors)

--- elk-reasoner/src/main/java/org/semanticweb/elk/reasoner/taxonomy/ClassTaxonomyHasher.java
+++ elk-reasoner/src/main/java/org/semanticweb/elk/reasoner/taxonomy/ClassTaxonomyHasher.java
@@ -22,6 +22,9 @@
  */
 package org.semanticweb.elk.reasoner.taxonomy;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.semanticweb.elk.owl.interfaces.ElkClass;
 import org.semanticweb.elk.util.hashing.HashGenerator;
 import org.semanticweb.elk.util.hashing.Hasher;
@@ -33,6 +36,7 @@ import org.semanticweb.elk.util.hashing.Hasher;
  * 
  * 
  * @author Frantisek Simancik
+ * @author Markus Kroetzsch
  */
 public class ClassTaxonomyHasher {
 
@@ -47,7 +51,40 @@ public class ClassTaxonomyHasher {
 		}
 	};
 
-	public static Hasher<ClassNode> classNodeHasher = new Hasher<ClassNode>() {
+	/**
+	 * Helper class for hashing a node of a class taxonomy. Implements some
+	 * caching to ensure good performance even when some nodes have unusually
+	 * many members.
+	 */
+	protected static Hasher<ClassNode> classNodeHasher = new Hasher<ClassNode>() {
+
+		/**
+		 * For nodes with at least this number of member classes, hash codes are
+		 * cached in the hashCache.
+		 */
+		final int cacheNodeMemberNo = 50;
+
+		/**
+		 * Maximum number of large class nodes for which the hash value is
+		 * cached.
+		 */
+		final int cacheMaxSize = 100;
+
+		/**
+		 * A simple Least Recently Used cache for the hashcodes of nodes that
+		 * have a particularly large number of members. There should not be many
+		 * such nodes in typical ontologies. Normally, this occurs if many
+		 * classes are equivalent to owl:Nothing due to some modelling error.
+		 */
+		final LinkedHashMap<ClassNode, Integer> hashCache = new LinkedHashMap<ClassNode, Integer>() {
+			private static final long serialVersionUID = 1;
+
+			@Override
+			protected boolean removeEldestEntry(
+					Map.Entry<ClassNode, Integer> eldest) {
+				return size() > cacheMaxSize;
+			}
+		};
 
 		public int hash(ClassNode node) {
 			int memberHash = HashGenerator.combineMultisetHash(true,
@@ -55,26 +92,50 @@ public class ClassTaxonomyHasher {
 
 			int subClassHash = "subClassOf".hashCode();
 			for (ClassNode o : node.getDirectSubNodes()) {
-				int subMemberHash = HashGenerator.combineMultisetHash(true,
-						o.getMembers(), elkClassHasher);
 				subClassHash = HashGenerator.combineMultisetHash(false,
-						subClassHash, subMemberHash);
+						subClassHash, getClassNodeMemberHash(o));
 			}
 
 			int superClassHash = "superClassOf".hashCode();
 			for (ClassNode o : node.getDirectSuperNodes()) {
-				int superMemberHash = HashGenerator.combineMultisetHash(true,
-						o.getMembers(), elkClassHasher);
 				superClassHash = HashGenerator.combineMultisetHash(false,
-						superClassHash, superMemberHash);
+						superClassHash, getClassNodeMemberHash(o));
 			}
 
 			return HashGenerator.combineListHash(memberHash, subClassHash,
 					superClassHash);
 		}
+
+		/**
+		 * Compute the hash for a class node. This method implements a simple
+		 * cache for nodes with unusually large numbers of members. This mainly
+		 * covers the case where a huge number of classes is equal to
+		 * owl:Nothing due to modelling errors.
+		 * 
+		 * @param classNode
+		 * @return
+		 */
+		private final int getClassNodeMemberHash(ClassNode classNode) {
+			if (classNode.getMembers().size() >= cacheNodeMemberNo) {
+				if (hashCache.containsKey(classNode)) {
+					return hashCache.get(classNode);
+				} else {
+					int hash = HashGenerator.combineMultisetHash(true,
+							classNode.getMembers(), elkClassHasher);
+					hashCache.put(classNode, hash);
+					return hash;
+				}
+			} else {
+				return HashGenerator.combineMultisetHash(true,
+						classNode.getMembers(), elkClassHasher);
+			}
+		}
 	};
 
-	public static Hasher<ClassTaxonomy> classTaxonomyHasher = new Hasher<ClassTaxonomy>() {
+	/**
+	 * Helper class for hashing a class taxonomy.
+	 */
+	protected static Hasher<ClassTaxonomy> classTaxonomyHasher = new Hasher<ClassTaxonomy>() {
 
 		public int hash(ClassTaxonomy taxonomy) {
 			return HashGenerator.combineMultisetHash(true, taxonomy.getNodes(),
@@ -82,6 +143,12 @@ public class ClassTaxonomyHasher {
 		}
 	};
 
+	/**
+	 * Compute the has code of a class taxonomy.
+	 * 
+	 * @param taxonomy
+	 * @return hash
+	 */
 	public static int hash(ClassTaxonomy taxonomy) {
 		return classTaxonomyHasher.hash(taxonomy);
 	}
