ef436fbad5b84c0d7b190cf0fa9a3860ecd96238
Markus Kroetzsch <mak@aifb.uni-karlsruhe.de>
2012-05-15 17:25:19 +0000

    Update popup messaging system to support selective filtering of message classes

---
+++ elk-util-parent/elk-util-logging/src/main/java/org/semanticweb/elk/util/logging/ElkMessage.java
@@ -0,0 +1,72 @@
+/*
+ * #%L
+ * ELK Utilities for Logging
+ * 
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2011 - 2012 Department of Computer Science, University of Oxford
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.semanticweb.elk.util.logging;
+
+import org.semanticweb.elk.util.hashing.HashGenerator;
+
+/**
+ * Enhanced message class that carries information to classify the message, in
+ * addition to the actual messages string.
+ * 
+ * @author Markus Kroetzsch
+ * 
+ */
+public class ElkMessage {
+
+	protected final String message;
+	protected final String messageType;
+
+	/**
+	 * Create a new message. The message can be an arbitrary string, possibly
+	 * with linebreaks (this is encouraged for very long messages). The
+	 * messageType should be a global identifier for the kind of message,
+	 * preferably human-readable and starting lowercase, such as
+	 * "unsupportedFeature". Dots could be used to relate a message type to a
+	 * Java package/class, but mentioning ELK is not useful (start only after
+	 * "org.semantiweb.elk").
+	 * 
+	 * @param message
+	 * @param messageType
+	 */
+	public ElkMessage(String message, String messageType) {
+		this.message = message;
+		this.messageType = messageType;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getMessageType() {
+		return messageType;
+	}
+
+	public String toString() {
+		return message;
+	}
+
+	public int hashCode() {
+		return HashGenerator.combinedHashCode(message, messageType);
+	}
+
+}
--- elk-util-parent/elk-util-logging/src/main/java/org/semanticweb/elk/util/logging/MessageDialogAppender.java
+++ elk-util-parent/elk-util-logging/src/main/java/org/semanticweb/elk/util/logging/MessageDialogAppender.java
@@ -22,7 +22,18 @@
  */
 package org.semanticweb.elk.util.logging;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.swing.Box;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
@@ -37,7 +48,13 @@ import org.apache.log4j.spi.LoggingEvent;
  * @author Markus Kroetzsch
  * 
  */
-public class MessageDialogAppender extends AppenderSkeleton {
+public class MessageDialogAppender extends AppenderSkeleton implements Runnable {
+
+	protected final ConcurrentLinkedQueue<LoggingEvent> eventBuffer = new ConcurrentLinkedQueue<LoggingEvent>();
+
+	protected final AtomicReference<String> messengerThreadName = new AtomicReference<String>("");
+	
+	protected final Set<String> ignoredMessageTypes = new HashSet<String>();
 
 	public MessageDialogAppender() {
 		super();
@@ -55,7 +72,10 @@ public class MessageDialogAppender extends AppenderSkeleton {
 
 	@Override
 	public void close() {
-		// Nothing to do.
+		// shutting down, just discard unreported events
+		synchronized (eventBuffer) {
+			eventBuffer.clear();
+		}
 	}
 
 	@Override
@@ -65,20 +85,75 @@ public class MessageDialogAppender extends AppenderSkeleton {
 
 	@Override
 	protected void append(LoggingEvent event) {
+		if (!Thread.currentThread().getName().equals(messengerThreadName)) {
+			eventBuffer.add(event);
+		} // else: recursive message creation is blocked; the messenger should
+			// not do this, but who knows
+
+		if (messengerThreadName.compareAndSet("", "Initialising thread ...")) {
+			Thread messengerThread = new Thread(this);
+			messengerThreadName.set(messengerThread.getName());
+			messengerThread.start();
+		}
+	}
+
+	protected void showMessage(LoggingEvent event) {
 		String messageTitle;
 		int messageLevel;
 		if (event.getLevel().isGreaterOrEqual(Level.ERROR)) {
-			messageTitle = "Error";
+			messageTitle = "ELK Error";
 			messageLevel = JOptionPane.ERROR_MESSAGE;
 		} else if (event.getLevel().isGreaterOrEqual(Level.WARN)) {
-			messageTitle = "Warning";
+			messageTitle = "ELK Warning";
 			messageLevel = JOptionPane.WARNING_MESSAGE;
 		} else {
-			messageTitle = "Information";
+			messageTitle = "ELK Information";
 			messageLevel = JOptionPane.INFORMATION_MESSAGE;
 		}
-		JOptionPane.showMessageDialog(null, event.getRenderedMessage(),
-				messageTitle, messageLevel);
+
+		Object Message = event.getMessage();
+		String messageType;
+		if (Message instanceof ElkMessage) {
+			messageType = ((ElkMessage) Message).getMessageType();
+			if (ignoredMessageTypes.contains(messageType)) {
+				return;
+			}
+		} else {
+			messageType = null;
+		}
+
+		JPanel radioPanel = new JPanel(new GridLayout(0, 1));
+		radioPanel.add(new JLabel(event.getRenderedMessage()));
+
+		JCheckBox ignoreMessageButton = new JCheckBox(
+				"Do not show further messages of this kind");
+		if (messageType != null) {
+			radioPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+			radioPanel.add(ignoreMessageButton);
+		}
+
+		// // Later, it will be possible to abort the reasoner here:
+		// Object[] options = { "Continue", "Abort Reaoner" };
+		// int result = JOptionPane.showOptionDialog(null, radioPanel,
+		// messageTitle,
+		// JOptionPane.DEFAULT_OPTION, messageLevel, null, options,
+		// options[0]);
+
+		JOptionPane.showMessageDialog(null, radioPanel, messageTitle,
+				messageLevel);
+
+		if (ignoreMessageButton.isSelected()) {
+			ignoredMessageTypes.add(messageType);
+		}
+
+	}
+
+	@Override
+	public void run() {
+		while (!eventBuffer.isEmpty()) {
+			showMessage(eventBuffer.poll());
+		}
+		messengerThreadName.set("");
 	}
 
 }
