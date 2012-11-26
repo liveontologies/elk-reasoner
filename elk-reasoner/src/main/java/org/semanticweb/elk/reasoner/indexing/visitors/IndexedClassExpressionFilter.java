c6d8db412beafc618ce3522e00ca1312a8a45a34
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2012-11-26 22:36:23 +0100

    should have been committed earlier

---
+++ elk-reasoner/src/main/java/org/semanticweb/elk/reasoner/indexing/visitors/IndexedClassExpressionFilter.java
@@ -0,0 +1,37 @@
+package org.semanticweb.elk.reasoner.indexing.visitors;
+/*
+ * #%L
+ * ELK Reasoner
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2011 - 2012 Department of Computer Science, University of Oxford
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.semanticweb.elk.reasoner.indexing.hierarchy.IndexedClass;
+import org.semanticweb.elk.reasoner.indexing.hierarchy.IndexedDataHasValue;
+import org.semanticweb.elk.reasoner.indexing.hierarchy.IndexedIndividual;
+import org.semanticweb.elk.reasoner.indexing.hierarchy.IndexedObjectIntersectionOf;
+import org.semanticweb.elk.reasoner.indexing.hierarchy.IndexedObjectSomeValuesFrom;
+
+public interface IndexedClassExpressionFilter extends
+		IndexedClassVisitor<IndexedClass>,
+		IndexedIndividualVisitor<IndexedIndividual>,
+		IndexedObjectIntersectionOfVisitor<IndexedObjectIntersectionOf>,
+		IndexedObjectSomeValuesFromVisitor<IndexedObjectSomeValuesFrom>,
+		IndexedDataHasValueVisitor<IndexedDataHasValue> {
+
+}
