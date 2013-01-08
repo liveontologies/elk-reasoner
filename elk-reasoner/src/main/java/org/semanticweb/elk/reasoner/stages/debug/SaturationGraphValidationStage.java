fa00f6fbf2c57bb046ad8c44906e4d66a18881e1
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2013-01-08 14:34:14 +0100

    fixed unbounded recursion in SaturationGraphValidationStage

--- elk-reasoner/src/main/java/org/semanticweb/elk/reasoner/stages/debug/SaturationGraphValidationStage.java
+++ elk-reasoner/src/main/java/org/semanticweb/elk/reasoner/stages/debug/SaturationGraphValidationStage.java
@@ -3,7 +3,9 @@
  */
 package org.semanticweb.elk.reasoner.stages.debug;
 
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -35,17 +37,18 @@ import org.semanticweb.elk.util.collections.ArrayHashSet;
  */
 public class SaturationGraphValidationStage extends BasePostProcessingStage {
 
-	private static final Logger LOGGER_ = Logger.getLogger(SaturationGraphValidationStage.class);
-	
+	private static final Logger LOGGER_ = Logger
+			.getLogger(SaturationGraphValidationStage.class);
+
 	private final OntologyIndex index_;
 	private final ClassExpressionValidator iceValidator_ = new ClassExpressionValidator();
 	private final ContextValidator contextValidator_ = new ContextValidator();
 	private final ContextRuleValidator ruleValidator_ = new ContextRuleValidator();
-	
+
 	public SaturationGraphValidationStage(final OntologyIndex index) {
 		index_ = index;
 	}
-	
+
 	@Override
 	public String getName() {
 		return "Saturation graph validation";
@@ -55,7 +58,12 @@ public class SaturationGraphValidationStage extends BasePostProcessingStage {
 	public void execute() throws ElkException {
 		// starting from indexed class expressions
 		for (IndexedClassExpression ice : index_.getIndexedClassExpressions()) {
-			iceValidator_.validate(ice);
+			iceValidator_.add(ice);
+		}
+		for (;;) {
+			if (iceValidator_.validate() || contextValidator_.validate())
+				continue;
+			break;
 		}
 	}
 
@@ -63,75 +71,111 @@ public class SaturationGraphValidationStage extends BasePostProcessingStage {
 	 * 
 	 */
 	private class ClassExpressionValidator {
-		
+
+		private final Queue<IndexedClassExpression> toValidate_ = new LinkedList<IndexedClassExpression>();
+
 		private final Set<IndexedClassExpression> cache_ = new ArrayHashSet<IndexedClassExpression>();
-		
-		void validate(IndexedClassExpression ice) {
-			if (!cache_.add(ice)) {
-				return;
+
+		void add(IndexedClassExpression ice) {
+			if (cache_.add(ice)) {
+				toValidate_.add(ice);
 			}
-			
+		}
+
+		boolean validate() {
+			if (toValidate_.isEmpty())
+				return false;
+			for (;;) {
+				IndexedClassExpression ice = toValidate_.poll();
+				if (ice == null)
+					break;
+				validate(ice);
+			}
+			return true;
+		}
+
+		private void validate(IndexedClassExpression ice) {
 			if (LOGGER_.isTraceEnabled()) {
 				LOGGER_.trace("Validating class expression " + ice);
 			}
-			
+
 			// this is the main check
 			if (!ice.occurs()) {
-				throw new ElkRuntimeException("Dead class expression detected " + ice);
+				throw new ElkRuntimeException("Dead class expression detected "
+						+ ice);
 			}
-			
+
 			// validating context
-			contextValidator_.validate(ice.getContext());
-			
+			contextValidator_.add(ice.getContext());
+
 			// validating context rules
 			LinkRule<Context> rule = ice.getCompositionRuleHead();
-			
+
 			while (rule != null) {
 				rule.accept(ruleValidator_, null, null);
 				rule = rule.next();
 			}
 		}
 	}
-	
+
 	/**
 	 * 
 	 * 
 	 */
 	private class ContextValidator {
-		
+
+		private final Queue<Context> toValidate_ = new LinkedList<Context>();
+
 		private final Set<Context> cache_ = new ArrayHashSet<Context>();
-		
-		void validate(Context context) {
-			if (context == null || !cache_.add(context)) {
-				return;
+
+		void add(Context context) {
+			if (context != null && cache_.add(context)) {
+				toValidate_.add(context);
 			}
-			
+		}
+
+		boolean validate() {
+			if (toValidate_.isEmpty())
+				return false;
+			for (;;) {
+				Context context = toValidate_.poll();
+				if (context == null)
+					break;
+				validate(context);
+			}
+			return true;
+		}
+
+		private void validate(Context context) {
+
 			if (LOGGER_.isTraceEnabled()) {
 				LOGGER_.trace("Validating context for " + context.getRoot());
 			}
-			
+
 			// validating subsumers recursively
 			for (IndexedClassExpression subsumer : context.getSubsumers()) {
-				iceValidator_.validate(subsumer);
+				iceValidator_.add(subsumer);
 			}
-			
-			//validating backward links
-			for (IndexedPropertyChain prop : context.getBackwardLinksByObjectProperty().keySet()) {
-				for (Context linkedContext : context.getBackwardLinksByObjectProperty().get(prop)) {
-					validate(linkedContext);
+
+			// validating backward links
+			for (IndexedPropertyChain prop : context
+					.getBackwardLinksByObjectProperty().keySet()) {
+				for (Context linkedContext : context
+						.getBackwardLinksByObjectProperty().get(prop)) {
+					add(linkedContext);
 				}
 			}
-			
-			//validating backward link rules
+
+			// validating backward link rules
 			LinkRule<BackwardLink> rule = context.getBackwardLinkRuleHead();
-			
+
 			while (rule != null) {
 				rule.accept(ruleValidator_, null, null);
 				rule = rule.next();
 			}
 		}
 	}
-	
+
 	/**
 	 * 
 	 */
@@ -144,27 +188,31 @@ public class SaturationGraphValidationStage extends BasePostProcessingStage {
 		}
 
 		@Override
-		public void visit(IndexedDisjointnessAxiom.ThisCompositionRule thisCompositionRule,
+		public void visit(
+				IndexedDisjointnessAxiom.ThisCompositionRule thisCompositionRule,
 				Writer writer, Context context) {
-			for (IndexedDisjointnessAxiom axiom : thisCompositionRule.getDisjointnessAxioms()) {
+			for (IndexedDisjointnessAxiom axiom : thisCompositionRule
+					.getDisjointnessAxioms()) {
 				if (!axiom.occurs()) {
-					throw new ElkRuntimeException("Dead disjointness axiom detected " + axiom);
+					throw new ElkRuntimeException(
+							"Dead disjointness axiom detected " + axiom);
 				}
-				
+
 				for (IndexedClassExpression ice : axiom.getDisjointMembers()) {
-					iceValidator_.validate(ice);
+					iceValidator_.add(ice);
 				}
 			}
-			
+
 		}
 
 		@Override
 		public void visit(
 				org.semanticweb.elk.reasoner.indexing.hierarchy.IndexedObjectIntersectionOf.ThisCompositionRule thisCompositionRule,
 				Writer writer, Context context) {
-			for (Map.Entry<IndexedClassExpression, IndexedObjectIntersectionOf> entry : thisCompositionRule.getConjunctionsByConjunct().entrySet()) {
-				iceValidator_.validate(entry.getKey());
-				iceValidator_.validate(entry.getValue());
+			for (Map.Entry<IndexedClassExpression, IndexedObjectIntersectionOf> entry : thisCompositionRule
+					.getConjunctionsByConjunct().entrySet()) {
+				iceValidator_.add(entry.getKey());
+				iceValidator_.add(entry.getValue());
 			}
 		}
 
@@ -172,32 +220,38 @@ public class SaturationGraphValidationStage extends BasePostProcessingStage {
 		public void visit(
 				org.semanticweb.elk.reasoner.indexing.hierarchy.IndexedSubClassOfAxiom.ThisCompositionRule thisCompositionRule,
 				Writer writer, Context context) {
-			for (IndexedClassExpression ice : thisCompositionRule.getToldSuperclasses()) {
-				iceValidator_.validate(ice);
+			for (IndexedClassExpression ice : thisCompositionRule
+					.getToldSuperclasses()) {
+				iceValidator_.add(ice);
 			}
-			
+
 		}
 
 		@Override
 		public void visit(
 				org.semanticweb.elk.reasoner.indexing.hierarchy.IndexedObjectSomeValuesFrom.ThisCompositionRule thisCompositionRule,
 				Writer writer, Context context) {
-			for (IndexedClassExpression ice : thisCompositionRule.getNegativeExistentials()) {
-				iceValidator_.validate(ice);
+			for (IndexedClassExpression ice : thisCompositionRule
+					.getNegativeExistentials()) {
+				iceValidator_.add(ice);
 			}
 		}
 
 		@Override
-		public void visit(IndexedDisjointnessAxiom.ThisContradictionRule thisContradictionRule,
+		public void visit(
+				IndexedDisjointnessAxiom.ThisContradictionRule thisContradictionRule,
 				Writer writer, Context context) {
 		}
 
 		@Override
-		public void visit(ForwardLink.ThisBackwardLinkRule thisBackwardLinkRule,
+		public void visit(
+				ForwardLink.ThisBackwardLinkRule thisBackwardLinkRule,
 				Writer writer, BackwardLink backwardLink) {
-			for (IndexedPropertyChain prop : thisBackwardLinkRule.getForwardLinksByObjectProperty().keySet()) {
-				for (Context context : thisBackwardLinkRule.getForwardLinksByObjectProperty().get(prop)) {
-					contextValidator_.validate(context);
+			for (IndexedPropertyChain prop : thisBackwardLinkRule
+					.getForwardLinksByObjectProperty().keySet()) {
+				for (Context context : thisBackwardLinkRule
+						.getForwardLinksByObjectProperty().get(prop)) {
+					contextValidator_.add(context);
 				}
 			}
 		}
@@ -206,18 +260,21 @@ public class SaturationGraphValidationStage extends BasePostProcessingStage {
 		public void visit(
 				Propagation.ThisBackwardLinkRule thisBackwardLinkRule,
 				Writer writer, BackwardLink backwardLink) {
-			for (IndexedPropertyChain prop : thisBackwardLinkRule.getPropagationsByObjectProperty().keySet()) {
-				for (IndexedClassExpression ice : thisBackwardLinkRule.getPropagationsByObjectProperty().get(prop)) {
-					iceValidator_.validate(ice);
+			for (IndexedPropertyChain prop : thisBackwardLinkRule
+					.getPropagationsByObjectProperty().keySet()) {
+				for (IndexedClassExpression ice : thisBackwardLinkRule
+						.getPropagationsByObjectProperty().get(prop)) {
+					iceValidator_.add(ice);
 				}
 			}
 		}
 
 		@Override
-		public void visit(Contradiction.ContradictionBackwardLinkRule bottomBackwardLinkRule,
-				Writer writer, BackwardLink backwardLink) {			
+		public void visit(
+				Contradiction.ContradictionBackwardLinkRule bottomBackwardLinkRule,
+				Writer writer, BackwardLink backwardLink) {
 		}
-		
+
 	}
 
 }
