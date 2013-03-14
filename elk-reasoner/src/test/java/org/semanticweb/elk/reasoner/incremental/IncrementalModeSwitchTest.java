8340929a3b0235f870607ffb38892935ae8e577f
Pavel Klinov <pavel.klinov@gmail.com>
2013-03-14 22:36:53 +0100

    a test showing that role need to be re-saturated even if no role axiom was added, just because some roles could be new

--- elk-reasoner/src/test/java/org/semanticweb/elk/reasoner/incremental/IncrementalModeSwitchTest.java
+++ elk-reasoner/src/test/java/org/semanticweb/elk/reasoner/incremental/IncrementalModeSwitchTest.java
@@ -2,6 +2,7 @@
  * 
  */
 package org.semanticweb.elk.reasoner.incremental;
+
 /*
  * #%L
  * ELK Reasoner
@@ -27,6 +28,12 @@ package org.semanticweb.elk.reasoner.incremental;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.semanticweb.elk.owl.exceptions.ElkException;
 import org.semanticweb.elk.owl.implementation.ElkObjectFactoryImpl;
@@ -35,8 +42,14 @@ import org.semanticweb.elk.owl.interfaces.ElkClass;
 import org.semanticweb.elk.owl.interfaces.ElkObjectFactory;
 import org.semanticweb.elk.owl.interfaces.ElkObjectProperty;
 import org.semanticweb.elk.owl.iris.ElkFullIri;
+import org.semanticweb.elk.owl.iris.ElkPrefix;
+import org.semanticweb.elk.owl.parsing.Owl2ParseException;
+import org.semanticweb.elk.owl.parsing.Owl2Parser;
+import org.semanticweb.elk.owl.parsing.Owl2ParserAxiomProcessor;
+import org.semanticweb.elk.owl.parsing.javacc.Owl2FunctionalStyleParserFactory;
 import org.semanticweb.elk.reasoner.Reasoner;
 import org.semanticweb.elk.reasoner.TestReasonerUtils;
+import org.semanticweb.elk.reasoner.stages.LoggingStageExecutor;
 import org.semanticweb.elk.reasoner.stages.PostProcessingStageExecutor;
 import org.semanticweb.elk.reasoner.taxonomy.model.Taxonomy;
 
@@ -51,13 +64,13 @@ import org.semanticweb.elk.reasoner.taxonomy.model.Taxonomy;
 public class IncrementalModeSwitchTest {
 
 	final ElkObjectFactory objectFactory = new ElkObjectFactoryImpl();
-	
+
 	@Test
 	public void testAddedTransitivity() throws ElkException {
 		TestChangesLoader loader = new TestChangesLoader();
 		TestChangesLoader changeLoader = new TestChangesLoader();
-		Reasoner reasoner = TestReasonerUtils
-				.createTestReasoner(loader, new PostProcessingStageExecutor());
+		Reasoner reasoner = TestReasonerUtils.createTestReasoner(loader,
+				new PostProcessingStageExecutor());
 
 		reasoner.setAllowIncrementalMode(false);
 		reasoner.registerOntologyChangesLoader(changeLoader);
@@ -66,24 +79,92 @@ public class IncrementalModeSwitchTest {
 		ElkClass B = objectFactory.getClass(new ElkFullIri(":B"));
 		ElkClass C = objectFactory.getClass(new ElkFullIri(":C"));
 		ElkClass D = objectFactory.getClass(new ElkFullIri(":D"));
-		ElkObjectProperty R = objectFactory.getObjectProperty(new ElkFullIri(":R"));
-		ElkAxiom axASubRB = objectFactory.getSubClassOfAxiom(A, objectFactory.getObjectSomeValuesFrom(R, B));
-		ElkAxiom axBSubRC = objectFactory.getSubClassOfAxiom(B, objectFactory.getObjectSomeValuesFrom(R, C));
-		ElkAxiom axRCSubD = objectFactory.getSubClassOfAxiom(objectFactory.getObjectSomeValuesFrom(R, C), D);
+		ElkObjectProperty R = objectFactory.getObjectProperty(new ElkFullIri(
+				":R"));
+		ElkAxiom axASubRB = objectFactory.getSubClassOfAxiom(A,
+				objectFactory.getObjectSomeValuesFrom(R, B));
+		ElkAxiom axBSubRC = objectFactory.getSubClassOfAxiom(B,
+				objectFactory.getObjectSomeValuesFrom(R, C));
+		ElkAxiom axRCSubD = objectFactory.getSubClassOfAxiom(
+				objectFactory.getObjectSomeValuesFrom(R, C), D);
 		ElkAxiom axTransR = objectFactory.getTransitiveObjectPropertyAxiom(R);
-		
+
 		loader.add(axASubRB).add(axBSubRC).add(axRCSubD);
 
 		Taxonomy<ElkClass> taxonomy = reasoner.getTaxonomyQuietly();
 
-		assertFalse(taxonomy.getNode(A).getAllSuperNodes().contains(taxonomy.getNode(D)));
-		
+		assertFalse(taxonomy.getNode(A).getAllSuperNodes()
+				.contains(taxonomy.getNode(D)));
+
 		reasoner.setAllowIncrementalMode(true);
 		changeLoader.add(axTransR);
 
 		taxonomy = reasoner.getTaxonomyQuietly();
 		// Now A should be a subclass of D since R is transitive
-		assertTrue(taxonomy.getNode(A).getAllSuperNodes().contains(taxonomy.getNode(D)));
+		assertTrue(taxonomy.getNode(A).getAllSuperNodes()
+				.contains(taxonomy.getNode(D)));
+	}
+
+	@Test
+	public void testSwitchDueToNewRole() throws ElkException, IOException {
+		String toAdd = "Prefix( : = <http://example.org/> ) Prefix( owl: = <http://www.w3.org/2002/07/owl#> ) Ontology(\n"
+				+ "SubClassOf(:C ObjectSomeValuesFrom(:T :B)) \n"
+				+ "ObjectPropertyDomain(:T owl:Nothing) \n" + ")";
+		String initial = "Prefix( : = <http://example.org/> )\n"
+				+ "Prefix( owl: = <http://www.w3.org/2002/07/owl#> ) \n"
+				+ "Ontology( \n" + "EquivalentClasses(:A :C) \n "
+				+ "SubClassOf(owl:Thing ObjectSomeValuesFrom(:R :B)) \n "
+				+ "SubClassOf(ObjectSomeValuesFrom(:S :B) :A) \n"
+				+ "SubObjectPropertyOf(:R :S) )";
+
+		List<ElkAxiom> ontology = loadAxioms(new StringReader(initial));
+		List<ElkAxiom> additions = loadAxioms(new StringReader(toAdd));
+		TestChangesLoader initialLoader = new TestChangesLoader();
+		TestChangesLoader changeLoader = new TestChangesLoader();
+
+		Reasoner reasoner = TestReasonerUtils.createTestReasoner(initialLoader,
+				new LoggingStageExecutor());
+
+		reasoner.setAllowIncrementalMode(false);
+		reasoner.registerOntologyChangesLoader(changeLoader);
+
+		for (ElkAxiom axiom : ontology) {
+			initialLoader.add(axiom);
+		}
+
+		assertFalse(reasoner.isInconsistent());
+		reasoner.getTaxonomy();
+
+		System.out.println("===========================================");
+
+		reasoner.setAllowIncrementalMode(true);
+
+		for (ElkAxiom add : additions) {
+			changeLoader.add(add);
+		}
+
+		assertTrue(reasoner.isInconsistent());
+	}
+
+	private List<ElkAxiom> loadAxioms(Reader reader) throws IOException,
+			Owl2ParseException {
+		Owl2Parser parser = new Owl2FunctionalStyleParserFactory()
+				.getParser(reader);
+		final List<ElkAxiom> axioms = new ArrayList<ElkAxiom>();
+
+		parser.accept(new Owl2ParserAxiomProcessor() {
+
+			@Override
+			public void visit(ElkPrefix elkPrefix) throws Owl2ParseException {
+			}
+
+			@Override
+			public void visit(ElkAxiom elkAxiom) throws Owl2ParseException {
+				axioms.add(elkAxiom);
+			}
+		});
+
+		return axioms;
 	}
 
 }
