4d28642770cdc146a7cdd7bee24c0ff0e0361745
Pavel Klinov <pavel.klinov@gmail.com>
2013-05-23 16:44:35 +0200

    branch for ORE workshop

---
+++ elk-cli/src/main/java/org/semanticweb/elk/cli/ORE.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.semanticweb.elk.cli;
+
+/**
+ * The CLI tool for the ORE 2013 contest
+ * 
+ * @author Pavel Klinov
+ *
+ * pavel.klinov@uni-ulm.de
+ */
+public class ORE {
+
+	public static void main(String... args) {
+		
+	}
+}
