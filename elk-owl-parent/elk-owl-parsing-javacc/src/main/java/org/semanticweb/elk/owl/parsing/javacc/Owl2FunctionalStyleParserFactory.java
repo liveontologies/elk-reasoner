a54691c4d1bd87362a8dbabc4fd931e43418fe05
Pavel Klinov <pavel.klinov@gmail.com>
2013-05-29 17:48:31 +0200

    various tweaks and improvements to the concurrent JavaCC lexer

--- elk-owl-parent/elk-owl-parsing-javacc/src/main/java/org/semanticweb/elk/owl/parsing/javacc/Owl2FunctionalStyleParserFactory.java
+++ elk-owl-parent/elk-owl-parsing-javacc/src/main/java/org/semanticweb/elk/owl/parsing/javacc/Owl2FunctionalStyleParserFactory.java
@@ -27,32 +27,142 @@ import java.io.Reader;
 
 import org.semanticweb.elk.owl.implementation.ElkObjectFactoryImpl;
 import org.semanticweb.elk.owl.interfaces.ElkObjectFactory;
+import org.semanticweb.elk.owl.iris.ElkPrefixDeclarations;
+import org.semanticweb.elk.owl.iris.ElkPrefixDeclarationsImpl;
 import org.semanticweb.elk.owl.parsing.Owl2Parser;
 import org.semanticweb.elk.owl.parsing.Owl2ParserFactory;
 
 public class Owl2FunctionalStyleParserFactory implements Owl2ParserFactory {
+
+	private final static JavaCCLexerFactory DEFAULT_LEXER_FACTORY = new ConcurrentJavaCCLexerFactory();
 
 	/**
 	 * the {@link ElkObjectFactory} used with this {@link Owl2ParserFactory}
 	 */
 	private final ElkObjectFactory objectFactory_;
 
-	public Owl2FunctionalStyleParserFactory(ElkObjectFactory objectFactory) {
+	private final JavaCCLexerFactory lexerFactory_;
+
+	public Owl2FunctionalStyleParserFactory(ElkObjectFactory objectFactory,
+			JavaCCLexerFactory lexerFactory) {
 		this.objectFactory_ = objectFactory;
+		this.lexerFactory_ = lexerFactory;
+	}
+
+	public Owl2FunctionalStyleParserFactory(ElkObjectFactory objectFactory) {
+		this(objectFactory, DEFAULT_LEXER_FACTORY);
 	}
 
 	public Owl2FunctionalStyleParserFactory() {
-		this.objectFactory_ = new ElkObjectFactoryImpl();
+		this(new ElkObjectFactoryImpl(), DEFAULT_LEXER_FACTORY);
 	}
 
 	@Override
 	public Owl2Parser getParser(InputStream stream) {
-		return new Owl2FunctionalStyleParser(stream, objectFactory_);
+		return new Owl2FunctionalStyleParser(stream, objectFactory_,
+				lexerFactory_);
 	}
 
 	@Override
 	public Owl2Parser getParser(Reader reader) {
-		return new Owl2FunctionalStyleParser(reader, objectFactory_);
+		return new Owl2FunctionalStyleParser(reader, objectFactory_,
+				lexerFactory_);
+	}
+
+	
+	/**
+	 * 
+	 * @author Pavel Klinov
+	 *
+	 * pavel.klinov@uni-ulm.de
+	 */
+	private static class Owl2FunctionalStyleParser extends AbstractOwl2FunctionalStyleParser {
+
+		private final ElkObjectFactory objectFactory_;
+
+		private final ElkPrefixDeclarations prefixDeclarations_;
+
+		private final JavaCCLexerFactory lexerFactory_;
+
+		private Owl2FunctionalStyleParser(InputStream stream,
+				ElkObjectFactory objectFactory,
+				ElkPrefixDeclarations prefixDeclarations,
+				JavaCCLexerFactory factory) {
+			super(stream);
+			this.objectFactory_ = objectFactory;
+			this.prefixDeclarations_ = prefixDeclarations;
+			this.lexerFactory_ = factory;
+
+			wrapLexer();
+		}
+
+		Owl2FunctionalStyleParser(InputStream stream,
+				ElkObjectFactory objectFactory, JavaCCLexerFactory lexerFactory) {
+			this(stream, objectFactory, new ElkPrefixDeclarationsImpl(),
+					lexerFactory);
+		}
+
+		Owl2FunctionalStyleParser(Reader reader,
+				ElkObjectFactory objectFactory,
+				ElkPrefixDeclarations prefixDeclarations,
+				JavaCCLexerFactory factory) {
+			super(reader);
+			this.objectFactory_ = objectFactory;
+			this.prefixDeclarations_ = prefixDeclarations;
+			this.lexerFactory_ = factory;
+
+			wrapLexer();
+		}
+
+		Owl2FunctionalStyleParser(Reader reader,
+				ElkObjectFactory objectFactory, JavaCCLexerFactory lexerFactory) {
+			this(reader, objectFactory, new ElkPrefixDeclarationsImpl(),
+					lexerFactory);
+		}
+
+		@Override
+		protected ElkPrefixDeclarations getElkPrefixDeclarations() {
+			return prefixDeclarations_;
+		}
+
+		@Override
+		protected ElkObjectFactory getElkObjectFactory() {
+			return this.objectFactory_;
+		}
+
+		@Override
+		public void ReInit(InputStream stream) {
+			super.ReInit(stream);
+
+			wrapLexer();
+		}
+
+		@Override
+		public void ReInit(InputStream stream, String encoding) {
+			super.ReInit(stream, encoding);
+
+			wrapLexer();
+		}
+
+		@Override
+		public void ReInit(Reader stream) {
+			super.ReInit(stream);
+
+			wrapLexer();
+		}
+
+		@Override
+		public void ReInit(AbstractOwl2FunctionalStyleParserTokenManager tm) {
+			super.ReInit(tm);
+
+			wrapLexer();
+		}
+
+		private void wrapLexer() {
+			token_source = lexerFactory_ != null ? lexerFactory_
+					.createLexer(token_source) : token_source;
+		}
+
 	}
 
 }
