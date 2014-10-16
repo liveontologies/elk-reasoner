5049d3199d86ea84e650034a224a7e585e6cc366
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2014-10-16 11:44:29 +0200

    Bugfix: ContradictionFromNegationRule should apply to decomposed subsumers to make it symmetric to IndexedObjectComplementOfDecomposition

--- elk-reasoner/src/main/java/org/semanticweb/elk/reasoner/saturation/rules/subsumers/ContradictionFromNegationRule.java
+++ elk-reasoner/src/main/java/org/semanticweb/elk/reasoner/saturation/rules/subsumers/ContradictionFromNegationRule.java
@@ -86,7 +86,7 @@ public class ContradictionFromNegationRule extends
 	@Override
 	public void apply(IndexedClassExpression premise, ContextPremises premises,
 			ConclusionProducer producer) {
-		if (negation_ != null && premises.getComposedSubsumers().contains(negation_)) {
+		if (negation_ != null && premises.getDecomposedSubsumers().contains(negation_)) {
 			//producer.produce(premises.getRoot(), ContradictionImpl.getInstance());
 			producer.produce(premises.getRoot(), new ContradictionFromNegation(negation_));
 		}
